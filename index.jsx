c7aa6ad2e9bb66683a7eb01c31d5bbc5cd8e40c1
Danil Savitsky <danildaniel121@gmail.com>
2023-09-28 22:27:59 +0400

    Moved animation utils out

---
+++ index.jsx
@@ -0,0 +1,5 @@
+import { render } from 'solid-js/web'
+import { App } from './src/components/SortVisualizer'
+import './src/assets/styles/index.css'
+
+render(() => <App />, document.getElementById('app'))
