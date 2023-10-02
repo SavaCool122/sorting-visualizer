baa158758e091041364583921af54f1f0f38dc4f
Daniil Savitskii <danildaniel121@gmail.com>
2023-10-02 19:44:56 +0400

    Fixed contacts

--- src/components/sidebar/Contacts.jsx
+++ src/components/sidebar/Contacts.jsx
@@ -2,14 +2,14 @@ import { Icon } from '../common/Icon'
 
 export function Contacts() {
 	return (
-		<div class="flex gap-x-4 p-2 self-center">
+		<div class="gap-x-4 self-center">
 			<a
-				class="s-contact inline-flex items-center gap-x-2 mb-5 p-2"
+				class="inline-flex items-center mb-5 gap-x-2 p-2"
 				target="_blank"
 				href="https://github.com/SavaCool122/sorting-visualizer"
 			>
 				Github
-				<Icon name="github" width={48} heigth={48} />
+				<Icon name="github" width={48} height={48} />
 			</a>
 		</div>
 	)
