50c895e26d2aa521b786d31d48b4b6be2bb71c88
Daniil Savitskii <46084440+SavaCool122@users.noreply.github.com>
2024-05-28 16:01:50 +0400

    feat: configurable speed (#28)

--- src/core/animations/start-animation.js
+++ src/core/animations/start-animation.js
@@ -3,12 +3,12 @@ import { delay } from './delay.js'
 
 /**
  * @param {[number, number][]} animations
- * @param {{onStep: ([number, number]) => void}} handlers
+ * @param {{onStep: Function, speed?: number}} handlers
  * @returns {Promise<void>}
  */
-export async function startAnimation(animations = [], { onStep }) {
+export async function startAnimation(animations = [], { speed = config.animationSpeed, onStep }) {
 	for (let i = 0; i < animations.length; i++) {
-		await delay(config.animationSpeed)
+		await delay(speed)
 		onStep(animations[i])
 	}
 	await delay(config.animationSpeed) // wait for the last animation
