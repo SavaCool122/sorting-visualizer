16e41845e79165f30af7cbf6d5fe0c916af3255e
Daniil Savitskii <danildaniel121@gmail.com>
2024-12-22 17:44:07 +0400

    updated icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+import { svgSpritemap as svg } from 'vite-plugin-svg-spritemap'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	plugins: [tailwindcss(), svelte()],
+	plugins: [svg({ pattern: 'icons/*.svg' }), tailwindcss(), svelte()],
 })
