f032c9fd3fd996179067e8e8fa5893096d062bee
Daniil Savitskii <danildaniel121@gmail.com>
2024-12-28 15:50:33 +0400

    bubble update

--- src/sorting/core/sorting-algorithms/bubble.js
+++ src/sorting/core/sorting-algorithms/bubble.js
@@ -1,33 +1,26 @@
-import { createRecordSwap } from '../animations/record-swap.js'
+/**
+ * @param {number[]} inputArray
+ * @param {(arr, i, j) => void} onSwap
+ * @returns {number[]}
+ */
+function bubbleSort(inputArray, onSwap = () => {}) {
+	const arr = [...inputArray]
+	const n = arr.length
 
-function bubble(arr) {
-	swapped = false
-	let end = arr.length - 1
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] > arr[i + 1]) {
-			swapped = true
-			swap(arr, i, i + 1)
+	for (let i = 0; i < n - 1; i++) {
+		for (let j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+				onSwap(arr, j, j + 1)
+			}
 		}
 	}
-	end--
+
+	return arr
 }
 
-let animations = []
-const recordSwap = createRecordSwap(animations)
-
-function swap(arr, index1, index2) {
-	recordSwap(index1, index2, (i, j) => {
-		const temp = arr[i]
-		arr[i] = arr[j]
-		arr[j] = temp
-	})
-}
-
-let swapped
-
-export function getBubbleSortAnimations(arr) {
-	do {
-		bubble(arr)
-	} while (swapped)
+export function getBubbleSortAnimations(arrayToSort) {
+	const animations = []
+	bubbleSort(arrayToSort, (_, i, j) => animations.push(i, j))
 	return animations
 }
