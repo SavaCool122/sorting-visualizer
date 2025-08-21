a20b8fba0f58c8d7bb0dd412c6ab8f1f65f2a251
Daniil Savitskii <danildaniel121@gmail.com>
2025-08-21 13:55:59 +0400

    Updated deps (#41)

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { preprocessMeltUI, sequence } from '@melt-ui/pp'
-import { compile } from 'svelte/compiler'
 
 export default {
 	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
-	compilerOptions: {
-		runes: true,
-	},
 }
